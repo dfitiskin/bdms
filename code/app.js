1da5fb6a011a06e4377f666f0f05a82cfc1d35fd
Dima Fitiskin <dfitiskin@gmail.com>
2016-02-09 09:40:18 +0400

    Add socket.io

--- code/app.js
+++ code/app.js
@@ -1,12 +1,16 @@
 var path = require('path'),
+	db = require(path.join(__dirname, 'server', 'services', 'db')),
+	api = require(path.join(__dirname, 'server', 'controllers', 'donators')), 
+	client = require(path.join(__dirname, 'server', 'controllers', 'client')),
 	app = require('express')(),
-	db = require(path.join(__dirname, 'server', 'services', 'db')); 
-	api = require(path.join(__dirname, 'server', 'controllers', 'donators')); 
-
-
+	server = require('http').Server(app);
+	
+app.io = require('socket.io')(server);
 app.use('/api/donators', api);
+app.use(client);
 
-var server = app.listen(process.env.PORT || (process.argv[2] || process.env.npm_package_config_port), function () {
+server.listen(process.env.PORT || (process.argv[2] || process.env.npm_package_config_port), function () {
 	db.connect();
 	console.log(server.address());
 });
+
